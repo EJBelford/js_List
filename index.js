86a67a3deedc6ef28cc734d21848e52d78280158
Gene Belford <EJBelford@comcast.net>
2022-10-04 15:53:37 -0400

    Updates for 10/4

--- index.js
+++ index.js
@@ -8,8 +8,8 @@
 //==============================================================================
 //--*----|----*----|----*----|----*----|----*----|----*----|----*----|----*----/
 //
-// Section ##: <title>
-// Lesson: ###
+// Section 24: Create Node Js Command Line Tools
+// Lesson: 331
 //
 //--*----1----*----2----*----3----*----4----*----5----*----6----*----7----*----8
 // NOTES: 
@@ -20,17 +20,25 @@
 //
 //--*----|----*----|----*----|----*----|----*----|----*----|----*----|----*----/
 
-const fs = require ('fs');
+const fs    = require('fs');
+const util  = require('util');
+const chalk = require('chalk');
 
 const prjctNm = "List"
-const debug   = 1;    // 0: Off   1: On
+const debug   = 0;    // 0: Off   1: On
 
 if (debug > 0) {
     console.log('DEBUG: Hi there from ' + prjctNm + '!');
     console.log('DEBUG: ' + prjctNm);
 };
 
-fs.readdir(process.cwd(), (err, fileNames) => {
+// Option 2 - Method #2
+// const lstat = util.promisify(fs.lstat);
+
+// Option 2 - Method 3
+const { lstat } = fs.promises; 
+
+fs.readdir(process.cwd(), async (err, fileNames) => {
     if (err) {
         if (debug > 0) {
             console.log('ERROR: ' + err);
@@ -38,9 +46,90 @@ fs.readdir(process.cwd(), (err, fileNames) => {
         };
     };
 
-    console.log('DEBUG: ' + fileNames); 
-    console.log(fileNames); 
+    // console.log('DEBUG: ' + fileNames); 
+    // console.log(fileNames); 
+
+    // Bad Code Example!!!!
+    /* for (let filename of filenames) {
+        fs.lstat(filename, (err, stats) => {
+            if (err) {
+                console.log('ERROR: ' + err);
+            }
+
+            console.log(filename, stats.isFile());
+        });
+    } */
+    // Bad Code Example!!!!
+
+    // Option 1
+    /* const allStats = Array(fileNames.length).fill(null); 
+
+    for (let filename of fileNames) {
+        const index = fileNames.indexOf(filename);
+        
+
+        fs.lstat(filename, (err, stats) => {
+            if (err) {
+                console.log('ERROR: ' + err);
+            }
+
+            allStats[index] = stats;
+
+            const ready = allStats.every((stats) => {
+                return stats;
+            });
+
+            if (ready) {
+                allStats.forEach((stats, index) => {
+                    console.log(fileNames[index], stats.isFile());
+                });
+            };
+        });
+    }  */
+    // Option 1
+
+    // Option 2
+       /*  for (let filename of fileNames) {
+            try {
+            const stats = await lstat(filename);
+
+            console.log(filename, stats.isFile());                
+            } catch (err) {
+                console.log('ERROR: ' + err);
+            }
+        } */
+    // Option 2
+    
+    // Option 3
+        const statPromises = fileNames.map(filename => {
+            return lstat(filename);
+        });
+
+        const allStats = await Promise.all(statPromises);
+
+        for (let stats of allStats) {
+            const index = allStats.indexOf(stats);
+
+            if (stats.isFile()) {
+                console.log(fileNames[index]);
+            } else {
+                console.log(chalk.blueBright(fileNames[index]));
+            }
+            //console.log(fileNames[index], stats.isFile());
+        }
+    // Option 3
+
 });
 
+// Option 2 - Method #1
+/* const lstat = (fileName) => {
+    return new Promise((resolve, reject) => {
+        fs.lstat(fileName, (err, stats) => {
+            if (err) {
+                reject(err);
+            };
 
-
+            resolve(stats);
+     });
+    });
+}; */
